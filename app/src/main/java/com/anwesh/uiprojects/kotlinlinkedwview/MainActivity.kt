76d93a95765cdf45dd397ccb7f15fdbf29c3d82b
Anwesh43 <anweshthecool0@gmail.com>
2018-06-16 12:37:56 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedwview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedwview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedwview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedwview.LinkedWView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedWView.create(this)
     }
 }
