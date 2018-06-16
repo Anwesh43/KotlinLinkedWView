d109c7a27fffbd7f4161b856a7371d50c47ac5c7
Anwesh43 <anweshthecool0@gmail.com>
2018-06-16 16:15:31 +0530

    adding Listener in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedwview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedwview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.kotlinlinkedwview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedwview.LinkedWView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedWView.create(this)
+        var view: LinkedWView = LinkedWView.create(this)
         fullScreen()
+        view.addLinkedWListener({
+            Toast.makeText(this, "completed ${it}", Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this, "${it} is reset", Toast.LENGTH_SHORT).show()
+        })
     }
 }
 
